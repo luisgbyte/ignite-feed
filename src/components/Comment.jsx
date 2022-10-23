2fc75bfedcbe89d7ec756e27120eee3aaee1b465
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-10-23 19:54:42 -0300

    add comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import style from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={style.comment}>
+      <img src="https://github.com/luisgbyte.png" alt="" />
+
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Luis</strong>
+              <time title="22 de Outubro às 11:00h" dateTime="2022-10-22">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
