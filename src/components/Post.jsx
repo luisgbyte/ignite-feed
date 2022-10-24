a2d19646c80c7fadde33b7f15c993ed4880b286f
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-10-24 17:18:56 -0300

    add form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,8 +36,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(event) {
-    event.preventDefault();
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
   }
 
   function deleteComment(commentToDelete) {
@@ -51,6 +55,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -93,10 +99,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           onChange={handleNewCommentChange}
           value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
